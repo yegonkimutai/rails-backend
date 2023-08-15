a0639d7e1228a605e0a226c21cdf8528c678568b
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-15 13:46:04 +0300

    :art:Update backend config

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_064027) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_103312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "messages", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  'habari hujambo',
+  'Hello, how are you?',
+  'Hola, cómo estás',
+  'Hallo, wie geht es dir?',
+  '你好吗',
+  'lumela u phela joang',
+  'Hello hoe gaan dit',
+  'Bonjour comment ça va',
+  'こんにちは、元気ですか',
+  'Hej hur mår du',
+  'Hei, hvordan går det',
+  'Ciao come stai',
+  'اسلام عليکم څنګه ياست',
+  'Привет, как дела?',
+  'Pẹlẹ o bawo ni'
+]
+
+greetings.each do |greeting|
+    Message.find_or_create_by(content: greeting)
+end
